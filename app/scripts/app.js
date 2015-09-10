aa4e7f12f50bf282b991596c716bd75d2dd03f00
pfitzpaddy <gis.patrick@gmail.com>
2015-09-10 18:07:07 +0430

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ngmReportHubApp
+ * @description
+ * # ngmReportHubApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ngmReportHub', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngmReportHubApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the ngmReportHub
+ */
+angular.module('ngmReportHub')
+	.controller('LoginCtrl', function () {
+		this.awesomeThings = [
+			'HTML5 Boilerplate',
+			'AngularJS',
+			'Karma'
+		];
+		
+		
+		
+	});
