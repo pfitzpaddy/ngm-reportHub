33aeaa1f7489f0eacc98aeddce88f9d91ac1a44a
pfitzpaddy <gis.patrick@gmail.com>
2015-09-11 13:45:24 +0430

    Styles custamisable from main app controller

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,21 +9,40 @@
  * Main module of the application.
  */
 angular
-  .module('ngmReportHub', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/login.html',
-        controller: 'LoginCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+	.module('ngmReportHub', [
+		'ngAnimate',
+		'ngCookies',
+		'ngResource',
+		'ngRoute',
+		'ngSanitize',
+		'ngTouch'
+  	])
+	.config(['$routeProvider', function ($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/login.html',
+				controller: 'LoginCtrl'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	}])
+	.controller('ngmReportHubCrtl', ['$scope', function ($scope) {
+
+		// ngm object
+		$scope.ngm = {
+			title: 'WHO Afghanistan',
+			logo: 'logo-who.png',
+			style: {
+				darkPrimaryColor: '#1976D2', // '#DE696E',
+				defaultPrimaryColor: '#2196F3', // '#EE6E73',
+				lightPrimaryColor: '#BBDEFB', //'#EF9A9A'
+				textPrimaryColor: '#FFFFFF',
+				accentColor: '#009688',
+				primaryTextColor: '#212121',
+				secondaryTextColor: '#727272',
+				dividerColor: '#B6B6B6'
+			}
+		}
+
+	}]);
