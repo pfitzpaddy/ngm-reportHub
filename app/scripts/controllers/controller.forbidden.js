4a4d6ec5745c6d35f87c8b0f4c24ab84b52aaba5
pfitzpaddy <gis.patrick@gmail.com>
2016-02-01 14:57:11 +0430

    bugfix - forbidden page

--- app/scripts/controllers/controller.forbidden.js
+++ app/scripts/controllers/controller.forbidden.js
@@ -14,35 +14,32 @@ angular.module('ngmReportHub')
 		];
 
 		// login object
-		$scope.forbidden = {
+		$scope.dashboard = {
 
 			// parent
 			ngm: $scope.$parent.ngm
 
 		}
 
-		// panel height = window height - header - padding
-		$scope.forbidden.ngm.style.height = $scope.forbidden.ngm.height - 160 - 10;
-		// panel padding = panel height - form height - footer - padding
-		$scope.forbidden.ngm.style.paddingHeight = ($scope.forbidden.ngm.style.height - 347 - 60 - 20) < 10 ? 10 : ($scope.forbidden.ngm.style.height - 347 - 60 - 20);
-		// get username (if available)
-		$scope.forbidden.username = ngmUser.get() ? ' ' + ngmUser.get().username : '';
+		// username
+		$scope.dashboard.username = ngmUser.get() ? ' ' + ngmUser.get().username : '';
 
 		// dews dashboard model
 		var model = {
+			name: 'dashboard_forbidden',
 			header: {
 				div: {
 					'class': 'col s12 m12 l12 report-header',
-					style: 'border-bottom: 3px ' + $scope.forbidden.ngm.style.defaultPrimaryColor + ' solid;'
+					style: 'border-bottom: 3px ' + $scope.dashboard.ngm.style.defaultPrimaryColor + ' solid;'
 				},
 				title: {
 					'class': 'col s12 m12 l12 report-title',
 					title: 'Forbidden!',
-					style: 'color: ' + $scope.forbidden.ngm.style.defaultPrimaryColor,
+					style: 'color: ' + $scope.dashboard.ngm.style.defaultPrimaryColor,
 				},
 				subtitle: {
 					'class': 'col s12 m12 l12 report-subtitle',
-					title: 'Sorry' + $scope.forbidden.username + ' you are not authorized to access this page, please contact the administrator',
+					title: 'Sorry' + $scope.dashboard.username + ', you are not authorized to access this page, please contact the administrator!',
 				}
 			},
 			rows: [{
@@ -51,17 +48,17 @@ angular.module('ngmReportHub')
 					widgets: [{
 						type: 'html',
 						card: 'card-panel',
-						style: 'padding:0px; height: ' + $scope.forbidden.ngm.style.height + 'px;',
+						style: 'padding:0px; height: ' + $scope.dashboard.ngm.style.height + 'px;',
 						config: {
-							style: $scope.forbidden.ngm.style,
-							template: '/scripts/widgets/ngm-html/template/forbidden.html'
+							style: $scope.dashboard.ngm.style,
+							templateUrl: '/scripts/widgets/ngm-html/template/forbidden.html'
 						}
 					}]
 				}]
 			}]
 		};
 
-		$scope.name = 'forbidden';
+		// assign model to scope
 		$scope.model = model;
 
 		// assign to ngm app scope
