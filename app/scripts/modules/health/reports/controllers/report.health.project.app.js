1e24941afe1ab21491643f8477b48bb40841c74a
pfitzpaddy <gis.patrick@gmail.com>
2016-03-01 14:43:16 +0700

    included app.js for health projects

---
+++ app/scripts/modules/health/reports/controllers/report.health.project.app.js
@@ -0,0 +1,173 @@
+/**
+ * @ngdoc function
+ * @name ngmReportHubApp.controller:ReportHealthProjectsCtrl
+ * @description
+ * # ReportHealthProjectsCtrl
+ * Controller of the ngmReportHub
+ */
+angular.module('ngmReportHub')
+	.controller('ReportHealthProjectAppCtrl', ['$scope', '$location', '$q', '$http', 'ngmData', 'ngmUser', function ($scope, $location, $q, $http, ngmData, ngmUser) {
+		this.awesomeThings = [
+			'HTML5 Boilerplate',
+			'AngularJS',
+			'Karma'
+		];
+
+		// report object
+		$scope.report = {
+
+			// ngm
+			ngm: $scope.$parent.ngm,
+
+			// form to add new project
+			newProjectUrl: '#/health/projects/details/new',
+
+			// province lists
+			provinceListRequest: $http({
+        method: 'GET',
+        url: 'http://' + $location.host() + '/api/health/getProvincesList'
+      }),
+
+			// district lists
+			districtListRequest: $http({
+        method: 'GET',
+        url: 'http://' + $location.host() + '/api/health/getDistrictsList'
+      }),
+
+			// hf type lists
+			hfTypeListRequest: $http({
+        method: 'GET',
+        url: 'http://' + $location.host() + '/api/health/getFacilityTypeList'
+      }),
+
+			// hf lists
+			hfListRequest: $http({
+        method: 'GET',
+        url: 'http://' + $location.host() + '/api/health/getFacilityList'
+      }),
+
+		}
+
+		// get all lists 
+		if ( !localStorage.getItem('lists') ) {
+
+			// send request
+			$q.all([$scope.report.provinceListRequest, $scope.report.districtListRequest, $scope.report.hfTypeListRequest, $scope.report.hfListRequest]).then(function(results){
+
+				// set lists to local storage
+				localStorage.setItem('lists', true);
+				localStorage.setItem('provinceList', JSON.stringify(results[0].data));
+				localStorage.setItem('districtList', JSON.stringify(results[1].data));
+				localStorage.setItem('hfTypeList', JSON.stringify(results[2].data));
+				localStorage.setItem('hfList', JSON.stringify(results[3].data));
+
+			});
+
+		}
+
+		// report dashboard model
+		$scope.model = {
+			name: 'report_health_list',
+			header: {
+				div: {
+					'class': 'col s12 m12 l12 report-header',
+					style: 'border-bottom: 3px ' + $scope.report.ngm.style.defaultPrimaryColor + ' solid;'
+				},
+				title: {
+					'class': 'col s12 m12 l12 report-title',
+					title: ngmUser.get().organization + ' | Health Projects',
+					style: 'color: ' + $scope.report.ngm.style.defaultPrimaryColor,
+				},
+				subtitle: {
+					'class': 'col s12 m12 l12 report-subtitle',
+					title: 'Health projects for ' + ngmUser.get().organization,
+				}
+			},
+			menu: [{
+				'icon': 'location_on',
+				'title': 'Projects',
+				'class': 'teal-text',
+				rows: [{
+					'title': 'Create New Project',
+					'class': 'grey-text text-darken-2 waves-effect waves-teal waves-teal-lighten-4',
+					'param': 'project',
+					'active': 'active',
+					'href': $scope.report.newProjectUrl
+				}]
+			}],
+			rows: [{
+				columns: [{
+					styleClass: 's12 m12 l12',
+					widgets: [{
+						type: 'html',
+						card: 'card-panel white grey-text text-darken-2',
+						config: {
+							html: '<a class="waves-effect waves-light btn" href="' + $scope.report.newProjectUrl + '"><i class="material-icons left">add_circle_outline</i>Add New Project</a>'
+						}
+					}]
+				}]
+			},{
+				columns: [{
+					styleClass: 's12 m12 l12',
+					widgets: [{
+						type: 'html',
+						card: 'card-panel white grey-text text-darken-2',
+						config: {
+							title: 'Active',
+							icon: 'edit',
+							newProjectUrl: $scope.report.newProjectUrl,
+							templateUrl: '/views/modules/health/views/health.project.list.html',							
+							request: {
+								method: 'POST',
+								url: 'http://' + $location.host() + '/api/health/project/getProjectList',
+								data: {
+									organization_id: ngmUser.get().organization_id,
+									project_status: 'active'
+								}
+							}
+						}
+					}]
+				}]
+			},{
+				columns: [{
+					styleClass: 's12 m12 l12',
+					widgets: [{
+						type: 'html',
+						card: 'card-panel white grey-text text-darken-2',
+						style: 'padding-bottom: 50px;',
+						config: {
+							title: 'Complete',
+							icon: 'done',
+							// page: true,
+							newProjectUrl: $scope.report.newProjectUrl,
+							templateUrl: '/views/modules/health/views/health.project.list.html',							
+							request: {
+								method: 'POST',
+								url: 'http://' + $location.host() + '/api/health/project/getProjectList',
+								data: {
+									organization_id: ngmUser.get().organization_id,
+									project_status: 'complete'
+								}
+							}
+						}
+					}]
+				}]
+			},{
+				columns: [{
+					styleClass: 's12 m12 l12',
+					widgets: [{
+						type: 'html',
+						card: 'card-panel',
+						style: 'padding:0px; height: 90px; padding-top:10px;',
+						config: {
+							html: $scope.report.ngm.footer
+						}
+					}]
+				}]
+			}]
+		};
+
+		// assign to ngm app scope
+		$scope.report.ngm.dashboard.model = $scope.model;
+		
+	}]);
