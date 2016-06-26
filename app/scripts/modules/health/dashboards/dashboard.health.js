7f2696a97a7c321b8c738c9340c38ba0ab1cef38
pfitzpaddy <gis.patrick@gmail.com>
2016-06-26 14:40:51 +0430

    HEALTH - updated dashboard PDF generate time

--- app/scripts/modules/health/dashboards/dashboard.health.js
+++ app/scripts/modules/health/dashboards/dashboard.health.js
@@ -296,7 +296,7 @@ angular.module('ngmReportHub')
 										downloadUrl: 'http://' + $location.host() + '/report/',
 										token: 'public',
 										viewportWidth: 1280,
-										pageLoadTime: 6600
+										pageLoadTime: 7600
 									}
 								},						
 								metrics: {
