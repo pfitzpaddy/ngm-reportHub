de2e7b101056f5a71e6d6c50033ea6b732a71be0
pfitzpaddy <gis.patrick@gmail.com>
2016-08-10 17:49:25 +0430

    HEALTH - Updated Admin organization list to include 'all'

--- app/scripts/modules/health/dashboards/dashboard.admin.js
+++ app/scripts/modules/health/dashboards/dashboard.admin.js
@@ -128,7 +128,7 @@ angular.module('ngmReportHub')
 																			 '/' + $scope.dashboard.endDate;
 
 							// update title to organization
-							if ( $route.current.params.organization === d.organization_id ) {
+							if ( $route.current.params.organization === d.organization_id && d.organization_id !== 'all' ) {
 								$scope.model.header.title.title += ' | ' + d.organization;
 								$scope.model.header.subtitle.title += ', ' + d.organization + ' organization';
 							}
