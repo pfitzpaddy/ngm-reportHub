c48cbc60b982ca9a3643de7b920eac8484f1777f
pfitzpaddy <pfitzgerald@immap.org>
2017-07-05 16:28:36 +0430

    Cluster: Set financial card to OCHA requirements

--- app/scripts/modules/cluster/reports/forms/cluster.project.form.financials.js
+++ app/scripts/modules/cluster/reports/forms/cluster.project.form.financials.js
@@ -53,6 +53,15 @@ angular.module( 'ngm.widget.project.financials', [ 'ngm.provider' ])
         // placeholder bydget activity
         lists: {
           reported_on_fts: [ { reported_on_fts_id: 'yes', reported_on_fts_name: 'Yes' }, { reported_on_fts_id: 'no', reported_on_fts_name: 'No' } ],
+          budget_funds: [ { budget_funds_id: 'financial', budget_funds_name: 'Financial' }, { budget_funds_id: 'inkind',budget_funds_name: 'InKind' } ],
+          financial_programming: [{ 
+            financial_programming_id: 'non_cash', financial_programming_name: 'Non-Cash' 
+          },{ 
+            financial_programming_id: 'restricted_cash', financial_programming_name: 'Restricted Cash' 
+          },{ 
+            financial_programming_id: 'unrestricted_cash', financial_programming_name: 'Unrestricted Cash' 
+          }],
+          multi_year_funding: [ { multi_year_funding_id: 'yes', multi_year_funding_name: 'Yes' }, { multi_year_funding_id: 'no', multi_year_funding_name: 'No' } ],
           activity_type: angular.copy( config.project.activity_type ),
           currencies: ngmClusterHelper.getCurrencies( config.project.admin0pcode )
         },
@@ -65,6 +74,13 @@ angular.module( 'ngm.widget.project.financials', [ 'ngm.provider' ])
             $budget.project_budget_date_recieved = 
                 moment( new Date( $budget.project_budget_date_recieved ) ).format('YYYY-MM-DD');
           }
+        },
+
+        // cancel
+        cancel: function() {
+          $timeout(function() {
+            $location.path( '/cluster/projects/summary/' + $scope.project.definition.id );
+          }, 400);
         },
 
         // donor
@@ -108,6 +124,101 @@ angular.module( 'ngm.widget.project.financials', [ 'ngm.provider' ])
           return selected.length ? selected[0].currency_name : 'No Selection!';
         },
 
+        // show in fts
+        showFunds: function( $data, $budget ) {
+          var selected = [];
+          $budget.budget_funds_id = $data;
+
+          // default
+          if( !$budget.reported_on_fts_id ){
+            $budget.budget_funds_id = 'financial';
+            $budget.budget_funds_name = 'Financial';
+          }
+
+          // selection
+          if( $budget.budget_funds_id ) {
+            selected = $filter('filter')( $scope.project.lists.budget_funds, { budget_funds_id: $budget.budget_funds_id }, true);
+            if( selected.length ) {
+              $budget.budget_funds_id = selected[0].budget_funds_id;
+              $budget.budget_funds_name = selected[0].budget_funds_name;
+            }
+          } 
+          return selected.length ? selected[0].budget_funds_name : 'N/A';
+        },
+
+        // show fts Id label
+        showProgrammingLabel: function(){
+          // budget progress
+          var show = false;
+          angular.forEach( $scope.project.definition.project_budget_progress, function( d, i ){
+            if ( d.budget_funds_id === 'financial') {
+              show = true;
+            }
+          });
+          return show;
+        },
+
+        // show in fts
+        showProgramming: function( $data, $budget ) {
+          var selected = [];
+          $budget.financial_programming_id = $data;
+
+          // default
+          if( !$budget.reported_on_fts_id ){
+            $budget.financial_programming_id = 'non_cash';
+            $budget.financial_programming_name = 'Non-Cash';
+          }
+
+          // selection
+          if( $budget.financial_programming_id ) {
+            selected = $filter('filter')( $scope.project.lists.financial_programming, { financial_programming_id: $budget.financial_programming_id }, true);
+            if( selected.length ) {
+              $budget.financial_programming_id = selected[0].financial_programming_id;
+              $budget.financial_programming_name = selected[0].financial_programming_name;
+            }
+          } 
+          return selected.length ? selected[0].financial_programming_name : 'N/A';
+        },
+
+        // funding 2017
+        showFunding2017: function(){
+          var show = false;
+          angular.forEach( $scope.project.definition.project_budget_progress, function( d, i ){
+            if ( d.multi_year_funding_id === 'yes') {
+              show = true;
+            }
+          });
+          return show;
+        },
+
+        // show in fts
+        showMultiYear: function( $data, $budget ) {
+          var selected = [];
+          $budget.multi_year_funding_id = $data;
+
+          // default
+          if( !$budget.multi_year_funding_id ){
+            $budget.multi_year_funding_id = 'no';
+            $budget.multi_year_funding_name = 'No';
+          }
+
+          // selection
+          if( $budget.multi_year_funding_id ) {
+            selected = $filter('filter')( $scope.project.lists.multi_year_funding, { multi_year_funding_id: $budget.multi_year_funding_id }, true);
+            if( selected.length ) {
+              $budget.multi_year_funding_id = selected[0].multi_year_funding_id;
+              $budget.multi_year_funding_name = selected[0].multi_year_funding_name;
+            }
+          } 
+
+          // multi-year set 
+          if ( $budget.multi_year_funding_id === 'no' ) {
+            $budget.funding_2017 = $budget.project_budget;
+          }
+
+          return selected.length ? selected[0].multi_year_funding_name : 'N/A';
+        },
+
         // show fts Id label
         showFtsIdLabel: function(){
           // budget progress
@@ -138,29 +249,23 @@ angular.module( 'ngm.widget.project.financials', [ 'ngm.provider' ])
               $budget.reported_on_fts_id = selected[0].reported_on_fts_id;
               $budget.reported_on_fts_name = selected[0].reported_on_fts_name;
             }
-          } 
+          }
           return selected.length ? selected[0].reported_on_fts_name : 'N/A';
         },
-
-        // cancel
-        cancel: function() {
-          $timeout(function() {
-            $location.path( '/cluster/projects/summary/' + $scope.project.definition.id );
-          }, 400);
-        },
-
         // add beneficiary
         addBudgetItem: function() {
           
           // inserted
           $scope.inserted = {
-            project_budget_date_recieved: moment().format('YYYY-MM-DD')
+            project_budget_amount_recieved: 0,
+            project_budget_date_recieved: moment().format('YYYY-MM-DD'),
+            comments: ''
           };
 
           // default donor
           if( $scope.project.definition.project_donor.length===1 ){
-            $scope.project.budget.project_donor_id = $scope.project.definition.project_donor[0].project_donor_id;
-            $scope.project.budget.project_donor_name = $scope.project.definition.project_donor[0].project_donor_name;
+            $scope.inserted.project_donor_id = $scope.project.definition.project_donor[0].project_donor_id;
+            $scope.inserted.project_donor_name = $scope.project.definition.project_donor[0].project_donor_name;
           }
 
           // clone
@@ -168,8 +273,7 @@ angular.module( 'ngm.widget.project.financials', [ 'ngm.provider' ])
           if ( length ) {
             var b = angular.copy( $scope.project.definition.project_budget_progress[ length - 1 ] );
             delete b.id;
-            $scope.inserted = angular.merge( $scope.inserted, b );
-            $scope.inserted.project_budget_amount_recieved = 0;
+            $scope.inserted = angular.merge( b, $scope.inserted );
           }
 
           // merge
@@ -202,7 +306,7 @@ angular.module( 'ngm.widget.project.financials', [ 'ngm.provider' ])
             }
           }).success( function( project ){
             // on success
-            Materialize.toast( 'Project Budget Progress Removed!', 3000, 'success');
+            Materialize.toast( 'Project Budget Item Removed!', 3000, 'success');
           }).error(function( err ) {
             // update
             Materialize.toast( 'Error!', 6000, 'error' );
