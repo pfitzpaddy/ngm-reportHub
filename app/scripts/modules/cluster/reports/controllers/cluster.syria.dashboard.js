9d572af1f485454c27cacde2c2648ecc91e1a5c6
pfitzpaddy <pfitzgerald@immap.org>
2018-04-11 12:24:47 +0300

    feature: syria dashboard demo

--- app/scripts/modules/cluster/reports/controllers/cluster.syria.dashboard.js
+++ app/scripts/modules/cluster/reports/controllers/cluster.syria.dashboard.js
@@ -28,23 +28,31 @@ angular.module('ngmReportHub')
 				$scope.model = {
 					name: 'cluster_syria_dashboard',
 					header: {
+						// div: {
+						// 	'class': 'col s12 m12 l12'
+						// }
 						div: {
-							'class': 'col s12 m12 l12'
+							'class': 'col s12 m12 l12 report-header',
+							style: 'border-bottom: 3px ' + $scope.report.ngm.style.defaultPrimaryColor + ' solid;'
+						},
+						title: {
+							'class': 'col s12 m12 l12 report-title truncate',
+							style: 'font-size: 3.4rem; color: ' + $scope.report.ngm.style.defaultPrimaryColor,
+							title: 'SYR | FSAC'
+						},
+						subtitle: {
+							'class': 'col s12 m12 l12 report-subtitle truncate hide-on-small-only',
+							'title': 'Overview for Syria'
 						}
 					},
+					menu: [{
+						'id': 'back-home',
+						'icon': 'keyboard_return',
+						'title': 'Back Home',
+						'class': 'teal lighten-1 white-text',
+						'href': '/desk/#/cluster/organization'
+					}],
 					rows: [{
-						columns: [{
-							styleClass: 's12 m12 l12',
-							widgets: [{
-								type: 'html',
-								card: 'white grey-text text-darken-2',
-								style: 'padding: 20px;',
-								config: {
-									html: '<a class="btn waves-effect waves-teal left" href="#/cluster/organization"><i class="material-icons left">keyboard_return</i>Back to Home</a><div style="padding-bottom:40px;"></div>'
-								}
-							}]
-						}]
-					},{
 						columns: [{
 							styleClass: 's12 m12 l12',
 							widgets: [{
@@ -81,5 +89,13 @@ angular.module('ngmReportHub')
 		}
 
 		$scope.report.init();
+
+		// timeout
+		setTimeout(function(){
+			$('#back-home').on('click', function(){
+				console.log('hello')
+				$location.path( '/cluster/organization' );
+			})
+		}, 2000);
 		
 	}]);
