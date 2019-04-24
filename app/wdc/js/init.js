c8bf37363cf166bd83bb1641c6caebce77f7e989
pfitzpaddy <pfitzgerald@immap.org>
2019-04-24 12:51:09 +0600

    feature: experiments with Tableau WDC

--- app/wdc/js/init.js
+++ app/wdc/js/init.js
@@ -1,7 +1,15 @@
 (function($){
   $(function(){
 
-    $('.sidenav').sidenav();
+	$('.sidenav').sidenav();
+
+	// set listeners
+	$( document ).ready(function () {
+		$( '#get-earthquake-data' ).click(function () {
+			tableau.connectionName = 'USGS Earthquake Feed';
+			tableau.submit();
+		});
+	});
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
