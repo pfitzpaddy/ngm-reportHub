99789255de0603117b238687392a2294969613d2
sebasnavarreteImmap <snavarrete@immap.org>
2019-05-02 14:39:12 -0500

    all organizations for all countries included Colombia

--- app/scripts/modules/cluster/reports/services/ngmClusterLists.js
+++ app/scripts/modules/cluster/reports/services/ngmClusterLists.js
@@ -709,16 +709,15 @@ angular.module( 'ngmReportHub' )
 
         var organizations;
 
-        if(admin0pcode === 'COL'){
+       /* if(admin0pcode === 'COL'){
 
          organizations = $filter('filter')(localStorage.getObject( 'lists' ).organizationsList,
                  {admin0pcode: 'COL'},true );
 
-        }else{
+        }else{*/
           organizations = localStorage.getObject( 'lists' ).organizationsList
-      }
+      //}
 
-          //console.log(organizations,"LAS ORGA");
 
 
           return organizations;
